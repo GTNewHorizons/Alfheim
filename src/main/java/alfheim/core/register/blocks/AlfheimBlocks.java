df1bbcaa093bd8d4083a5ffc99980cea20b1e38d
BlesseNtumble <vitalya_migun@mail.ru>
2017-08-16 20:21:36 +0900

    Update .gitignore

---
+++ src/main/java/alfheim/core/register/blocks/AlfheimBlocks.java
@@ -0,0 +1,19 @@
+package alfheim.core.register.blocks;
+
+public class AlfheimBlocks {
+
+	public static void initialize() 
+	{
+		registration();
+		oreDictRegistration();
+	}
+	
+	private static void registration()
+	{
+		
+	}
+	
+	private static void oreDictRegistration()
+	{
+	}
+}
