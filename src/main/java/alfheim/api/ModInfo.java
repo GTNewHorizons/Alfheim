002243d3ce31bd54d839d79c0c6a74e6b379580b
AlexSocol <Sashadee2012@mail.ru>
2019-02-25 01:11:38 +0200

    changed version

--- src/main/java/alfheim/api/ModInfo.java
+++ src/main/java/alfheim/api/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 	
 	public static final String MAJOR = "BETA";
 	//public static final String MINOR = "";
-	public static final String BUILD = "pre17.1";
+	public static final String BUILD = "pre17.2";
 	
 	public static final String MODID = "alfheim";
 	public static final String NAME = "Alfheim";
