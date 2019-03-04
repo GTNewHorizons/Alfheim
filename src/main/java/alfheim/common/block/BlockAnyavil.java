6311ec6cf0677e6be552d694efb36f78226c7efb
AlexSocol <Sashadee2012@mail.ru>
2019-03-04 16:41:08 +0200

    fixes

--- src/main/java/alfheim/common/block/BlockAnyavil.java
+++ src/main/java/alfheim/common/block/BlockAnyavil.java
@@ -28,29 +28,31 @@ import vazkii.botania.api.lexicon.ILexiconable;
 import vazkii.botania.api.lexicon.LexiconEntry;
 import vazkii.botania.api.mana.IManaTrigger;
 import vazkii.botania.api.wand.IWandHUD;
+import vazkii.botania.common.block.ModBlocks;
 
 public class BlockAnyavil extends BlockContainer implements IManaTrigger, IWandHUD, ILexiconable {
-
+	
 	public BlockAnyavil() {
 		super(Material.iron);
-		this.setBlockName("Anyavil");
-		this.setCreativeTab(AlfheimCore.alfheimTab);
-		this.setLightOpacity(0);
-		this.setHardness(5.0F);
-		this.setResistance(2000.0F);
-		this.setStepSound(soundTypeAnvil);
+		setBlockName("Anyavil");
+		setBlockTextureName("botania:storage2");
+		setCreativeTab(AlfheimCore.alfheimTab);
+		setLightOpacity(0);
+		setHardness(5.0F);
+		setResistance(2000.0F);
+		setStepSound(soundTypeAnvil);
 	}
-
+	
 	@Override
 	public boolean renderAsNormalBlock() {
 		return false;
 	}
-
+	
 	@Override
 	public boolean isOpaqueCube() {
 		return false;
 	}
-
+	
 	@Override
 	public int getRenderType() {
 		return LibRenderIDs.idAnyavil;
@@ -61,7 +63,7 @@ public class BlockAnyavil extends BlockContainer implements IManaTrigger, IWandH
 		int l = MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 0.5) & 3;
 		world.setBlockMetadataWithNotify(x, y, z, l, 3);
 	}
-
+	
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		ItemContainingTileEntity te = (ItemContainingTileEntity) world.getTileEntity(x, y, z);
@@ -87,24 +89,24 @@ public class BlockAnyavil extends BlockContainer implements IManaTrigger, IWandH
 		}
 		return true;
 	}
-
+	
 	@Override
 	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
 		int l = world.getBlockMetadata(x, y, z) & 3;
-
+		
 		if (l != 3 && l != 1) {
 			this.setBlockBounds(0.125F, 0.0F, 0.0F, 0.875F, 1.0F, 1.0F);
 		} else {
 			this.setBlockBounds(0.0F, 0.0F, 0.125F, 1.0F, 1.0F, 0.875F);
 		}
 	}
-
+	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public int getMixedBrightnessForBlock(IBlockAccess world, int x, int y, int z) {
 		return world.getLightBrightnessForSkyBlocks(x, y, z, world.getBlock(x, y, z).getLightValue(world, x, y - 1, z));
 	}
-
+	
 	@Override
 	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
 		ItemContainingTileEntity te = (ItemContainingTileEntity) world.getTileEntity(x, y, z);
@@ -113,7 +115,7 @@ public class BlockAnyavil extends BlockContainer implements IManaTrigger, IWandH
 			world.spawnEntityInWorld(entityitem);
 			te.setItem(null);
 		}
-
+		
 		super.breakBlock(world, x, y, z, block, meta);
 	}
 	
@@ -146,14 +148,14 @@ public class BlockAnyavil extends BlockContainer implements IManaTrigger, IWandH
 		if (tile != null && tile instanceof TileAnyavil) ((TileAnyavil) tile).onBurstCollision(burst, world, x, y, z);
 		world.notifyBlocksOfNeighborChange(x, y, z, this);
 	}
-
+	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void renderHUD(Minecraft mc, ScaledResolution res, World world, int x, int y, int z) {
 		TileEntity tile = world.getTileEntity(x, y, z);
 		if (tile != null && tile instanceof TileAnyavil) ((TileAnyavil) tile).renderHUD(mc, res, world, x, y, z);
 	}
-
+	
 	@Override
 	public LexiconEntry getEntry(World world, int x, int y, int z, EntityPlayer player, ItemStack lexicon) {
 		return AlfheimLexiconData.anyavil;
