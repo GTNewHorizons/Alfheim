00fa53ea0346dd44eab637e8c01da172f8f8feb7
AlexSocol <Sashadee2012@mail.ru>
2019-12-28 17:36:19 +0200

    Changed snow armor model

--- src/main/java/alfmod/client/render/entity/RenderEntityDedMoroz.kt
+++ src/main/java/alfmod/client/render/entity/RenderEntityDedMoroz.kt
@@ -1,14 +1,14 @@
 package alfmod.client.render.entity
 
 import alfmod.AlfheimModularCore
-import alfmod.client.render.model.ModelDedMoroz
 import cpw.mods.fml.relauncher.*
+import net.minecraft.client.model.ModelBiped
 import net.minecraft.client.renderer.entity.RenderBiped
 import net.minecraft.entity.*
 import net.minecraft.util.ResourceLocation
 
 @SideOnly(Side.CLIENT)
-class RenderEntityDedMoroz: RenderBiped(ModelDedMoroz(), 0.5f) {
+class RenderEntityDedMoroz: RenderBiped(ModelBiped(), 0.5f) {
 	
 	val texture = ResourceLocation("${AlfheimModularCore.MODID}:textures/entity/DedMoroz.png")
 	
--- src/main/java/alfmod/common/item/equipment/armor/ItemSnowArmor.kt
+++ src/main/java/alfmod/common/item/equipment/armor/ItemSnowArmor.kt
@@ -2,12 +2,15 @@ package alfmod.common.item.equipment.armor
 
 import alfheim.common.core.util.AlfheimTab
 import alfmod.AlfheimModularCore
+import alfmod.client.render.model.ModelSnowArmor
 import alfmod.common.core.helper.IconHelper
 import alfmod.common.item.AlfheimModularItems
 import cpw.mods.fml.relauncher.*
+import net.minecraft.client.model.ModelBiped
 import net.minecraft.client.renderer.texture.IIconRegister
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.ItemStack
+import net.minecraft.item.*
 import net.minecraft.util.StatCollector
 import net.minecraftforge.common.util.EnumHelper
 import vazkii.botania.common.Botania
@@ -18,6 +21,12 @@ open class ItemSnowArmor(type: Int, name: String): ItemManasteelArmor(type, name
 	
 	companion object {
 		val snow = EnumHelper.addArmorMaterial("snow", 25, intArrayOf(2, 6, 5, 2), 16)!!
+		
+		var model1 = ModelSnowArmor(1f)
+		var model2 = ModelSnowArmor(0.5f)
+		var model3 = ModelBiped()
+		
+		var model: ModelBiped = model1
 	}
 	
 	init {
@@ -25,7 +34,7 @@ open class ItemSnowArmor(type: Int, name: String): ItemManasteelArmor(type, name
 	}
 	
 	override fun getArmorTextureAfterInk(stack: ItemStack?, slot: Int) =
-		"${AlfheimModularCore.MODID}:textures/model/snow${if (ConfigHandler.enableArmorModels) "New" else if (slot == 2) "1" else "0"}.png"
+		"${AlfheimModularCore.MODID}:textures/model/snow${if (slot == 3) "0" else if (ConfigHandler.enableArmorModels) "New" else if (slot == 2) "1" else "0"}.png"
 	
 	@SideOnly(Side.CLIENT)
 	override fun registerIcons(reg: IIconRegister) {
@@ -52,4 +61,39 @@ open class ItemSnowArmor(type: Int, name: String): ItemManasteelArmor(type, name
 	
 	override fun getArmorSetTitle(player: EntityPlayer?) =
 		"${StatCollector.translateToLocal("botaniamisc.armorset")} $armorSetName (${getSetPiecesEquipped(player)}/${armorSetStacks.size})"
+	
+	@SideOnly(Side.CLIENT)
+	override fun getArmorModel(entityLiving: EntityLivingBase, itemStack: ItemStack, armorSlot: Int): ModelBiped? {
+		model = when (armorSlot) {
+			0 -> model2
+			1 -> model1
+			2 -> model2
+			3 -> model3
+			else -> model
+		}
+		
+		model.bipedHead.showModel = armorSlot == 0
+		model.bipedHeadwear.showModel = armorSlot == 0
+		model.bipedBody.showModel = armorSlot == 1 || armorSlot == 2
+		model.bipedRightArm.showModel = armorSlot == 1
+		model.bipedLeftArm.showModel = armorSlot == 1
+		model.bipedRightLeg.showModel = armorSlot == 2 || armorSlot == 3
+		model.bipedLeftLeg.showModel = armorSlot == 2 || armorSlot == 3
+		model.isSneak = entityLiving.isSneaking
+		model.isRiding = entityLiving.isRiding
+		model.isChild = entityLiving.isChild
+		model.aimedBow = false
+		model.heldItemRight = if (entityLiving.heldItem != null) 1 else 0
+		
+		if (entityLiving is EntityPlayer && entityLiving.itemInUseDuration > 0) {
+			val enumaction = entityLiving.getItemInUse().itemUseAction
+			if (enumaction == EnumAction.block) {
+				model.heldItemRight = 3
+			} else if (enumaction == EnumAction.bow) {
+				model.aimedBow = true
+			}
+		}
+		
+		return model
+	}
 }
