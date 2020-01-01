8fcd4c7085da1ab00b444e923267bf96f7d3998e
AlexSocol <Sashadee2012@mail.ru>
2020-01-01 21:59:15 +0200

    Reverting flugel AI (1/8)

--- src/main/java/alfheim/common/entity/boss/ai/flugel/constant/AIChangeStage.kt
+++ src/main/java/alfheim/common/entity/boss/ai/flugel/constant/AIChangeStage.kt
@@ -7,7 +7,7 @@ class AIChangeStage(flugel: EntityFlugel): AIConstantExecutable(flugel) {
 	
 	override fun execute() {
 		if (flugel.AI.stage > EntityFlugel.Companion.STAGE.INIT) {
-			if (flugel.AI.stage < EntityFlugel.Companion.STAGE.MAGIC && flugel.health <= flugel.maxHealth * 0.6) {
+			if (flugel.AI.stage < EntityFlugel.Companion.STAGE.MAGIC && flugel.health <= flugel.maxHealth) {
 				flugel.AI.stage = EntityFlugel.Companion.STAGE.MAGIC
 			}
 		}
